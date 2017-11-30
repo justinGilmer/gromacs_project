c0a1c326b02cc282449445838ba15ce96e1317a8
Justin Gilmer <justin.b.gilmer@vanderbilt.edu>
2017-11-29 18:00:07 -0600

    More flavor text, modify file permissions

---
+++ run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# master bash script to run the two simulations
+
+# run the water simulation
+
+# run the pentane MoSDeF simulation
+
+
+# let user know it was submitted
+cat <<END
+Success! The water box simulation and pentane box simulation
+were successfully submitted!
+
+These jobs will take a some time to run, especially with everyone in
+class submitting.
+
+We will continue the presentation soon, and will check back on these
+later! You can logout for now!
+END
